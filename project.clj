0bf7024c3e3f2d520f87ef314626fc188c9a5ff2
Hugh Aristotle Powell <hugh@hughpowell.co.uk>
2017-05-09 20:10:10 +0100

    Change license to MPLv2

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject railway-oriented-programming "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Mozilla Public License v2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
