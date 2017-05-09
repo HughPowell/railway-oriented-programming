179c03ede89e4842b270fc56ce32bf85c4fdebf8
Hugh Aristotle Powell <hugh@hughpowell.co.uk>
2017-05-09 20:30:55 +0100

    Upgrade to Clojure 1.9.0-alpha16

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Mozilla Public License v2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]])
