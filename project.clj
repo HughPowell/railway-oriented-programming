7d8026467e2da2cc87eaa1cd75e2bfbfe8370f0c
Hugh Aristotle Powell <hugh@hughpowell.co.uk>
2017-07-20 21:34:59 +0100

    Move version to next SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject railway-oriented-programming "0.1.1"
+(defproject railway-oriented-programming "0.1.2-SNAPSHOT"
   :description "An implementation of railway oriented programming"
   :url "https://github.com/HughPowell/railway-oriented-programming"
   :license {:name "Mozilla Public License v2.0"
