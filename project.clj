9c39bc63147f4d3e261f7ab75fbb4d3b50f29626
Hugh Aristotle Powell <hugh@hughpowell.co.uk>
2017-07-20 20:39:47 +0100

    Add automatic deployment

--- project.clj
+++ project.clj
@@ -1,8 +1,23 @@
-(defproject railway-oriented-programming "0.1.0"
+(defproject railway-oriented-programming "0.1.1-SNAPSHOT"
   :description "An implementation of railway oriented programming"
   :url "https://github.com/HughPowell/railway-oriented-programming"
   :license {:name "Mozilla Public License v2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]]
+  :plugins [[update-readme "0.1.0"]]
+  :repositories [["releases" {:url "https://clojars.org/repo/"
+                              :username :env
+                              :password :env
+                              :sign-releases false}]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
-  :monkeypatch-clojure-test false)
+  :monkeypatch-clojure-test false
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["update-readme"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["update-readme"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
